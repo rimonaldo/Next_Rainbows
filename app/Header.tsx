77fe60092b73ec9e641bfe6d4c841bad02c3dddf
Rimon <rimons2804@gmail.com>
2022-12-30 12:48:07 +0200

    header style changes

--- app/Header.tsx
+++ app/Header.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 
 function Header() {
    return (
-      <header className="p-5 bg-blue-500">
-         <Link className='px-2 py-1 bg-white text-blue-500 rounded-lg' href="/">Home</Link>
+      <header className="p-5 bg-blue-500 ">
+         <Link className=' m-2 px-2 py-1 bg-white text-blue-500 rounded-lg' href="/">Home</Link>
          <Link href="/todos" className='px-2 py-1 bg-white text-blue-500 rounded-lg'>Todos</Link>
       </header>
    )
