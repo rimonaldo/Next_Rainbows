6fb3b574e9b6ad6a21c41528cca82bd5ec6410fb
Rimon <rimons2804@gmail.com>
2022-12-30 12:58:41 +0200

    page not found fallback

--- app/todos/[todoId]/page.tsx
+++ app/todos/[todoId]/page.tsx
@@ -14,6 +14,8 @@ const fetchTodo = async (todoId: string) => {
 
 async function TodoPage({ params: { todoId } }: PageProps) {
    const todo = await fetchTodo(todoId)
+
+   if (!todo.id) return notFound()
    return (
       <div className="p-10 bg-yellow-200 border-2 m-2 shadow-lg">
          <div>TodoPage</div>
