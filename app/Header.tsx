e447f78762f58a92dbdc786f6b2c5bd413327f48
Rimon <rimons2804@gmail.com>
2022-12-31 11:45:28 +0200

    added search route

--- app/Header.tsx
+++ app/Header.tsx
@@ -4,8 +4,9 @@ import React from 'react'
 function Header() {
    return (
       <header className="p-5 bg-blue-500 ">
-         <Link className=' m-2 px-2 py-1 bg-white text-blue-500 rounded-lg' href="/">Home</Link>
-         <Link href="/todos" className='px-2 py-1 bg-white text-blue-500 rounded-lg'>Todos</Link>
+         <Link className='px-2 py-1 bg-white text-blue-500 rounded-lg' href="/">Home</Link>
+         <Link href="/todos" className='m-2 px-2 py-1 bg-white text-blue-500 rounded-lg'>Todos</Link>
+         <Link href="/search" className='px-2 py-1 bg-white text-blue-500 rounded-lg'>Search</Link>
       </header>
    )
 }
