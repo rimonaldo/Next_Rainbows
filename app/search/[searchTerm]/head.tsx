f627df5826e3b2536e31615518743f9aeb9acaa4
Rimon <rimons2804@gmail.com>
2022-12-31 11:49:20 +0200

    search results page

---
+++ app/search/[searchTerm]/head.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function head() {
+  return (
+    <title>the Search Page</title>
+  )
+}
---
+++ app/search/[searchTerm]/page.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+
+type PageProps = {
+   params: {
+      searchTerm: string
+   }
+}
+
+type SearchResult = {
+   organic_results: [
+      {
+         position: number
+         title: string
+         link: string
+         thumbnail: string
+         snippet: string
+      }
+   ]
+}
+
+const search = async (searchTerm: string) => {
+   process.env.API_KEY
+   const res = await fetch(`https://serpapi.com/search.json?q=${searchTerm}&api_key=${process.env.API_KEY}`)
+//    throw new Error('WHOOPSS...Something broke')
+   const data: SearchResult = await res.json()
+   return data
+}
+
+async function SearchResults({ params: { searchTerm } }: PageProps) {
+   const searchResults = await search(searchTerm)
+
+   return (
+      <div>
+         <ol>
+            {searchResults.organic_results.map(result => (
+               <li key={result.position} className="list-decimal">
+                  <p className="font-bold">{result.title}</p>
+                  <p>{result.snippet}</p>
+               </li>
+            ))}
+         </ol>
+      </div>
+   )
+}
+
+export default SearchResults
